5aec9a53865262bc2df356c614ff66caf03ece3e
NeroWolf001 <118631622+NeroWolf001@users.noreply.github.com>
2022-11-20 20:02:28 -0500

    Updated to v1.0.1

--- ScrewYourScaleCompensation.cs
+++ ScrewYourScaleCompensation.cs
@@ -1,24 +1,26 @@
 ﻿using FrooxEngine;
 using FrooxEngine.CommonAvatar;
-using FrooxEngine.LogiX.Data;
 using HarmonyLib;
 using NeosModLoader;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ScrewYourScaleCompensation
 {
-    public class Patch : NeosMod
+    public class ScrewYourScaleCompensation : NeosMod
     {
         public override string Name => "ScrewYourScaleCompensation";
         public override string Author => "NeroWolf & LeCloutPanda";
-        public override string Version => "1.0.0";
+        public override string Version => "1.0.1";
+
+        public static ModConfiguration config;
+
+        [AutoRegisterConfigKey]
+        public static ModConfigurationKey<bool> enabled = new ModConfigurationKey<bool>("Enabled", "", () => true);
 
         public override void OnEngineInit()
         {
+            config = GetConfiguration();
+            config.Save();
+
             Harmony harmony = new Harmony($"dev.{Author}.{Name}");
             harmony.PatchAll();
         }
@@ -30,19 +32,14 @@ namespace ScrewYourScaleCompensation
             [HarmonyPostfix]
             static void FixScaleCompensation(AvatarAudioOutputManager __instance, Sync<float> ____scaleCompensation)
             {
-                // Run this after 3 game ticks to ensure __instance isn't null
                 __instance.RunInUpdates(3, () =>
                 {
-                    // Attach non persistent comps(They won't save with the avatar etc)
-                    var copy = __instance.Slot.AttachComponent<ValueCopy<float>>(true);
-                    var value = __instance.Slot.AttachComponent<ValueRegister<float>>(true);
-                    // Set defaults
-                    value.Value.Value = 1f;
-                    copy.Persistent = false;
-                    value.Persistent = false;
-                    // Assign the fucking copy to ScaleCompensation value
-                    copy.Source.Value = value.Value.ReferenceID;
-                    copy.Target.Value = ____scaleCompensation.ReferenceID;
+                    if (!config.GetValue(enabled)) return;
+
+                    var valueUserOverride = __instance.Slot.AttachComponent<ValueUserOverride<float>>();
+                    valueUserOverride.Target.Value = ____scaleCompensation.ReferenceID;
+                    valueUserOverride.Default.Value = 1f;
+                    valueUserOverride.Persistent = false;
                 });
             }
         }
